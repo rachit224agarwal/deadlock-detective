89e503b3bcfab8f370ec2dea4f1ff1897d516b5f
rachit224agarwal <agarwalrachit224@gmail.com>
2025-11-18 22:43:26 +0530

    Day:5 connect Server --> Client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
+  
+  const checkDeadlock = async () => {
+    
+    const graph = {
+      P1 : ["R1"],
+      R1 : ["P2"],
+      P2 : ["R2"],
+      R2 : ["P1"]
+    };
+
+    const result = await fetch('http://localhost:8080/check', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({graph})
+    });
+
+    const data = await result.json();
+    console.log(data);
+
+  }
 
   return (
     <>
-     <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div>
+       <h1 className="text-3xl font-bold underline">Deadlock Detection</h1>
+       <button className="bg-slate-500 rounded-lg p-3" onClick={checkDeadlock}>Check Deadlock</button>
+    </div>
     </>
   )
 }
